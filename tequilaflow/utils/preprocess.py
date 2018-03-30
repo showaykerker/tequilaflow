aae2f20a5b84b9cdb68b46c9373d1c26db2d58a3
showay <df405102@gmail.com>
2018-03-30 14:11:25 +0800

    ADD: to_catagorized

---
+++ tequilaflow/utils/preprocess.py
@@ -0,0 +1,34 @@
+from __future__ import absolute_import
+import numpy as np
+import copy
+
+def to_catagorize(Y, n = None):
+	label = {}
+	for i,v in enumerate(np.unique(Y)): label[v] = i
+	if len(label) != n: raise ValueError('len(label) != n')
+	ret = []
+	for y in Y[0]:
+		vec = [0] * n
+		vec[label[y]] = 1
+		ret.append(copy.deepcopy(vec))
+
+	ret = np.reshape(ret, (1, len(Y[0]), n))
+	return ret, label
+
+def reverse_catagorize(ret, label):
+	res = dict((v,k) for k,v in label.items())
+	Y = []
+	for i in ret[0]:
+		y = res[np.argmax(i)]
+		Y.append(y)
+	Y = np.reshape(Y, (1, len(Y)))
+
+	return Y
+
+
+if __name__ == '__main__':
+	a = np.array([['apple', 'cat', 'banana', 'showay', 'fish']])
+	a, label = to_catagorize(a, 5)
+	print(a)
+	a = reverse_catagorize(a, label)
+	print(a)
