FIX: activation.softmax and loss.cross_entropy (probably)

tequilaflow/core/activations.py
@@ -62,8 +62,8 @@ class Softmax(Output):
 		self.sub_type = 'Softmax'
 
 	def kernel(self, X_): 
-		X_ret = np.zeros(X.shape)
-		for i,row in enumerate(X):
+		X_ret = np.zeros(X_.shape)
+		for i,row in enumerate(X_):
 			exps = np.exp(row-row.max())
 			X_ret[i] = exps/np.sum(exps)
 		return X_ret
@@ -72,12 +72,6 @@ class Softmax(Output):
 		# https://stats.stackexchange.com/questions/235528/backpropagation-with-softmax-cross-entropy
 		# https://en.wikipedia.org/wiki/Activation_function
 		X_ret = copy.deepcopy(X_)
-		for vec in X_ret: # [0.88, 0.12]
-			fi = self.kernel(np.expand_dims(X_ret, axis=0))
-			fy = copy.deepcopy(fi)
-
-
-
 		return X_ret
 
 	def forward(self, X_): 

tequilaflow/core/loss.py
@@ -145,27 +145,18 @@ class cross_entropy(loss_function):
 		super().__init__()
 
 	def get_vector(self, Y_predict, Y_true, batch_size=None, pass_=False):
-		tot_loss = 0
-		if not pass_: 
-			super().get_vector(Y_predict, Y_true, batch_size)
-			if batch_size == None : raise ValueError('batch_size is None')
-			else: self.batch_size = batch_size
-
-		for y_p, y_t in zip(Y_predict, Y_true):
-			err = y_p - y_t
-			inside_ = 0 
-			for k in range(len(y_t)): 
-				inside_ += y_t * np.log(y_p+1) 
-			tot_loss += inside_ 
-		#print('~~',tot_loss)
-		tot_loss = np.expand_dims(tot_loss, axis=0)
-		#input(('showay ', tot_loss))
-		return tot_loss
+		nclass = Y_predict.shape[1]
+		E = - (Y_true * np.log(Y_predict)).sum(axis=1)
+		E = np.expand_dims(E, axis=1)
+		E = np.append(E, E, axis=1)
+		return E
 
 	def get_pCpy(self, Y_predict, Y_true, idx=None):
-		ret = super().get_pCpy(Y_predict, Y_true, self.get_vector, idx=idx)[0]
+		ret = np.expand_dims((Y_predict-Y_true)[idx], axis=0)
 		return ret
 
 	def get_performance(self, Y_pred, Y_true):
 		ret = self.get_vector(Y_pred, Y_true, batch_size=None)
+		#print(ret)
+		#input(ret.mean())
 		return ret.mean()